7f96d2cc2e4812b75ad24d065d5f828299c04e50
TheGamingDolphinyt <81319103+TheGamingDolphinyt@users.noreply.github.com>
2022-04-21 16:28:18 +0100

    Coppied from already working env

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:lets_chat/screens/screens.dart';
+import 'package:lets_chat/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.light(),
+      darkTheme: AppTheme.dark(),
+      themeMode: ThemeMode.dark,
+      title: 'Lets Chat',
+      home: HomeScreen(),
+    );
+  }
+}
